f6cc1bf9299694080c2c4f5a880cede04908e0e3
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-03-06 12:52:04 +0530

    new

--- myproj/src/UseState.js
+++ myproj/src/UseState.js
@@ -1,7 +1,10 @@
-import {useState} from "react";
+import {useEffect,useState} from "react";
 export default function UseState()
 {
     let [counter,updateCounter]=useState(0);
+    useEffect(()=>{
+        console.log("This is Coding ");
+    })
 return(
 <>
 <h3>Counter Value is:{counter}</h3>
